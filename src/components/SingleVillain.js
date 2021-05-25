7181e32c61bcd4ca97cc6af7d8c0a989d428e5b6
Tej Pratap <kumarpratap.1447@gmail.com>
2021-05-25 14:35:45 +0530

    add villain page

---
+++ src/components/SingleVillain.js
@@ -0,0 +1,43 @@
+import React,{Fragment} from 'react';
+
+const singleVillain = (props) => {
+    const{name,appearance,biography,images,powerstats} = props.villain;
+    return (
+        <Fragment>
+            <div class="row">
+                <div class="col s12 m7">
+                <div class="card pd-1">
+                <div class="card-image">
+                <img src={images.lg} style={{"height":"225px","width":"325px"}}/>
+                
+                
+                <h3>{name}</h3>
+                <h5> Full Name : {biography.fullName}</h5>
+                <hr/>
+
+                <div className="content-div">
+                <h5>Gender : {appearance.gender}</h5>
+                <h5>Height : {appearance.height[0]}</h5>
+                <h5>Weight : {appearance.weight[1]}</h5>         
+                <h5> Combat : {powerstats.combat}</h5>
+                <h5>Durability : {powerstats.durability}</h5>
+                <h5>Strength : {powerstats.strength}</h5>
+                <h5>Intelligence : {powerstats.intelligence}</h5>
+                <h5>Power : {powerstats.power}</h5>
+                <h5>Speed : {powerstats.speed}</h5>
+                </div>
+                </div>
+
+                <div class="card-action">
+                <h5>Publisher : {biography.publisher}</h5>
+                </div>
+
+                </div>
+                </div>
+            </div>
+        </Fragment>
+       
+    )
+}
+
+export default singleVillain;
--- src/components/Villain.js
+++ src/components/Villain.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React,{Fragment,useState,useEffect} from 'react';
+import axios from 'axios';
+import SingleVillain from './SingleVillain';
+import Search from './Search';
 
 const Villain = () => {
+
+
+    const [villains,setVillains] = useState([]);
+    
+    //console.log(heroes);
+
+    useEffect(()=>{
+        //console.log("use effect fired");
+        async function loadFunction(){
+            const res = await axios.get(`https://superhero-search.p.rapidapi.com/api/villains`,{
+            headers:{
+                'x-rapidapi-key':'c55a71dfdbmsh99fb2531a44af40p188352jsnf277366026c0'
+            }
+        });
+
+        //console.log(res.data);
+        setVillains(res.data);
+        }
+        loadFunction();
+
+        
+
+    // eslint-disable-next-line
+    },[]);
+
     return (
-        <div>
-            <p>This is villain page</p>
-        </div>
+        <Fragment>
+        <Search/>
+        {villains == null ? <h1>Loading...</h1> : <div style={villainStyle}>
+                {villains.map((villain)=>{
+                    return <SingleVillain villain={villain} key={villain.id}/>
+                })}
+                
+            </div>}
+            
+        </Fragment>
+        
     )
+};
+
+const villainStyle={
+    display:'grid',
+    gridTemplateColumns:'repeat(3 ,1fr)',
+    gridGap:'0.5rem'
 }
 
 export default Villain;
